c125afabdeeca517f5c6738853171a2af1aba7ed
007vict <super_vg@bk.ru>
2019-03-13 23:25:54 +0300

    deploy blog setting on heroku,change static url

--- blog_by_example/settings.py
+++ blog_by_example/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/blog/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 2525
